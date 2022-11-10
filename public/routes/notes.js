1cfec1e37c7ade0e18e720e36d4bfae97943ed27
Nllano777 <nllbr99@gmail.com>
2022-11-09 21:55:35 -0600

    added delete note method

--- public/routes/notes.js
+++ public/routes/notes.js
@@ -1,9 +1,9 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
 notes.get('/', (req, res) => {
-    readFromFile('db/notes-db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('db/notes-db.json').then((allNotes) => res.json(JSON.parse(allNotes)));
 });
 
 notes.post('/', (req, res) => {
@@ -21,6 +21,22 @@ notes.post('/', (req, res) => {
     } else {
         res.error('Cannot add empty note');
     }
+});
+
+notes.delete('/:id', (req, res) => {
+    readFromFile('db/notes-db.json')
+        .then((allNotes) => {
+            var id = req.params.id;
+            allNotes = JSON.parse(allNotes);
+            for (var i = 0; i < allNotes.length; i++) {
+                if (allNotes[i].id === id) {
+                    allNotes.splice(i, 1);
+                }
+            }
+            writeToFile('db/notes-db.json', allNotes);
+            res.json('Note deleted');
+        })
+        .catch(() => res.error('Unable to delete note'));
 });
 
 module.exports = notes;
